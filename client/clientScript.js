46cba355749c24c40667fdea45508304982cd402
Karl Taguibao <karltag2018@gmail.com>
2021-07-04 17:39:03 +0800

    updated working ver

--- client/clientScript.js
+++ client/clientScript.js
@@ -1,16 +1,41 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server);
+// io is the one that is connecting
+const socket = io();
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+const form = document.querySelector(".typing-area .typing-box");
+const divContainer = document.getElementsByClassName("message-area");
+
+console;
+
+form.addEventListener("keydown", function (e) {
+  if (e.key === "Enter") {
+    console.log("HMMM", JSON.stringify(form.textContent));
+    socket.emit("chat message", form.textContent);
+    form.textContent = "";
+
+    e.preventDefault();
+  }
 });
-io.on("connection", (socket) => {
-  console.log("a user connected");
+console.log("KEYS", Object.keys(socket));
+
+console.log(`${socket.id} connected`);
+socket.on("disconnect", () => {
+  console.log(`${socket.id} disconnected`);
 });
-server.listen(3000, () => {
-  console.log("listening *:3000");
+
+socket.on("chat message", ({ msg, id }) => {
+  console.log("RECEIVED FROM SERVER", msg);
+  const divContainer = document.getElementsByClassName("message-area")[0];
+  divContainer.innerHTML += `
+    <div class="wrapper-message">
+        <div class="username ${id === socket.id ? "you" : "user"}">${
+    id === socket.id ? "you" : id
+  }</div>
+        <div class="message ${id === socket.id ? "you" : "user"}">${msg}</div>
+    </div>
+    `;
+  setTimeout(() => {
+    divContainer.scrollTop = divContainer.scrollHeight;
+  }, 300);
 });
+
+console.log(testy);
--- serverScript.js
+++ serverScript.js
@@ -5,6 +5,21 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+io.on("connection", (socket) => {
+  console.log(`${socket.id} connected`);
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+  });
+
+  socket.on("chat message", (msg) => {
+    console.log("FROM CLIENT");
+    io.emit("chat message", {
+      msg,
+      id: socket.id,
+    });
+  });
+});
+
 //for the css static
 app.use(express.static(__dirname + "/client"));
 // app.use("/client", express.static(__dirname + "/client"));
@@ -19,17 +34,6 @@ app.use("/server", express.static(__dirname + "/server"));
 app.get("/", function (req, res) {
   //./ means current directory
   res.sendFile("index.html", { root: "./client" });
-});
-
-io.on("connection", (socket) => {
-  console.log(`${socket.id} connected`);
-  socket.on("disconnect", () => {
-    console.log(`${socket.id} disconnected`);
-  });
-
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-  });
 });
 
 server.listen(3000, () => {
